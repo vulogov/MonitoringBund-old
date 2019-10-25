4c2557b3f14676f0e5295d441dbf5d73a2de78e9
Vladimir Ulogov <vladimir.ulogov@me.com>
2019-10-25 00:52:12 -0700

    Initial submission

---
+++ ctx/context.go
@@ -0,0 +1,17 @@
+package bund_context
+
+import (
+  "github.com/common-nighthawk/go-figure"
+  "github.com/hashicorp/go-uuid"
+)
+
+var Version = "MonitoringBund version 0.1.1 (development release)"
+var Version_Num = "0.1.1"
+var Version_Release = "(development release)"
+var Logo = figure.NewFigure("[theBund> ", "o8", true)
+var PS1 = "[BUND> "
+
+var (
+  CfgFile     string
+  Logverbose  string
+)
